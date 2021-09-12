809a8c3892f3a4da9ea90bfeb844e8d9522e2272
Florent <florent.git@zeteo.me>
2021-09-12 14:49:16 +0200

    First commit

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,16 +5,29 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="deb1 deb2 php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
+pkg_dependencies="postgresql"
+# Node version
+NODEJS_VERSION=14
 
 #=================================================
 # PERSONAL HELPERS
 #=================================================
 
-#=================================================
-# EXPERIMENTAL HELPERS
-#=================================================
+install_node_deps() {
+    pushd "$final_path"
+        ynh_use_nodejs
+        ynh_exec_warn_less sudo -u $app env $ynh_node_load_PATH npm install
+    popd
+}
 
-#=================================================
-# FUTURE OFFICIAL HELPERS
-#=================================================
+run_registration() {
+    pushd "$final_path"
+        ynh_exec_warn_less sudo -u $app env $ynh_node_load_PATH NODE_ENV=production npm run start -- -r -c $config_path -f "$app.yaml"
+    popd
+}
+
+setup_final_path_rights() {
+    chmod 750 "$final_path"
+    chmod -R o-rwx "$final_path"
+    chown -R $app:$app "$final_path"
+}
