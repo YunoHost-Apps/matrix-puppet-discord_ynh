77e34a05798cb7e8839f7bbe212791bed5feacfd
Florent <florent.git@zeteo.me>
2021-09-17 16:21:24 +0200

    Various fixes after code review

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -12,33 +12,3 @@ NODEJS_VERSION=14
 #=================================================
 # PERSONAL HELPERS
 #=================================================
-
-install_node_deps() {
-    pushd "$final_path"
-        ynh_use_nodejs
-        ynh_exec_warn_less sudo -u $app env $ynh_node_load_PATH npm install
-    popd
-}
-
-run_registration() {
-    pushd "$final_path"
-        ynh_exec_warn_less sudo -u $app env $ynh_node_load_PATH NODE_ENV=production $final_path/run.sh -r -f "$app.yaml"
-    popd
-}
-
-setup_final_path_rights() {
-    chmod 750 "$final_path"
-    chmod -R o-rwx "$final_path"
-    chown -R $app:$app "$final_path"
-}
-
-setup_run_script_rights() {
-    chmod 750 "$final_path/run.sh"
-    chown "$app:$app" "$final_path/run.sh"
-}
-
-create_etc() {
-  local etc_path=$1
-  mkdir -p -m 750 "$etc_path"
-  chown "$app:$app" "$etc_path"
-}
