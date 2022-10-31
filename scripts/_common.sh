5f7fcc005d84755b53e70cee99ed654225843e96
kay (they/it) <gh@juke.fr>
2022-10-31 13:36:06 +0100

    update to latest

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,10 +5,10 @@
 #=================================================
 
 # Node version
-NODEJS_VERSION=16
+NODEJS_VERSION=12
 
 # dependencies used by the app (must be on a single line)
-pkg_dependencies="postgresql"
+pkg_dependencies="postgresql libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev"
 
 #=================================================
 # PERSONAL HELPERS
